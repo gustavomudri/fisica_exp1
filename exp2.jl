c3fa6e1dc96dadfcf10caad53d668ba20711a488
gustamudri <gustamudri@gmail.com>
2022-10-16 17:08:16 -0300

    exp2

--- exp2.jl
+++ exp2.jl
@@ -1,5 +1,7 @@
 using PlotlyJS,DataFrames
 
+
+
 tempos_metalica=[
 0.531
 0.530
@@ -73,11 +75,15 @@ plot(df,layout)
 
 df2 = scatter(y=t_2,x=h,name="Bola de 4,23cm",mode="markers")
 df3 = scatter(y=[0.530],x=[137.42],name="Bola de 2cm",mode="markers")
-df4 = 
+A = [ones(5) h]
+B =(A'* A)\(A't_2)
+df4 = scatter(x=[0,140],y=B[1].+B[2].*[0,140],name="ajuste linear para a bola 4,23cm",mode="lines")
 layout2 = Layout(title="Modelo linear Altura x Tempo",
                    xaxis_title="Altura(cm)",
                    yaxis_title="Tempo(s)",
                    xaxis=attr(dtick=20.,tick0=0.000),
                    yaxis=attr(dtick=0.05,tick0=0.000),
                    )
-plot([df2,df3],layout2)
+plot([df2,df3,df4],layout2)
+B[1]
+B[2]
