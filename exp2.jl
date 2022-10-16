e9c45d4ef798b2b82dbf8b64fbb936ce1693306f
gustamudri <gustamudri@gmail.com>
2022-10-16 17:06:46 -0300

    exp2

--- exp2.jl
+++ exp2.jl
@@ -1,5 +1,83 @@
-using PlotlyJS
+using PlotlyJS,DataFrames
 
-dados=[50,51,51,51,51,52]
+tempos_metalica=[
+0.531
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.530
+0.529
+0.529
+0.529
+0.530
+0.530
+0.530
+0.530
+0.529
+0.530
+0.530
+0.530
+0.529
+0.530
+0.530
+0.530
+0.530
+0.530
+]
 
-plot(histogram(x=dados,xbins_size=0.001,))
+df= histogram(
+    x=tempos_metalica,
+    xaxis=0.529:0.001:0.532
+) 
+
+layout = Layout(
+    title="Tempos de queda da bola metalica na altura de 137.42 cm",
+    xaxis=attr(dtick=0.001,tick0=0.001),
+    yaxis=attr(dtick=5,tick0=0),
+    xaxis_title="Tempo(s)",
+    yaxis_title="Repetição",
+    )
+plot(df,layout)
+
+    h=[133.18,108.910,72.280,53.740,17.340]
+    t_2=[0.576,0.492,0.429,0.370,0.209].^2
+
+
+df2 = scatter(y=t_2,x=h,name="Bola de 4,23cm",mode="markers")
+df3 = scatter(y=[0.530],x=[137.42],name="Bola de 2cm",mode="markers")
+df4 = 
+layout2 = Layout(title="Modelo linear Altura x Tempo",
+                   xaxis_title="Altura(cm)",
+                   yaxis_title="Tempo(s)",
+                   xaxis=attr(dtick=20.,tick0=0.000),
+                   yaxis=attr(dtick=0.05,tick0=0.000),
+                   )
+plot([df2,df3],layout2)
